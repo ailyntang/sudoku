1cd5ba585dd85dceb05736e1e3ac6236f87f079b
Ai-Lyn Tang <ailyntang@gmail.com>
2020-04-26 14:38:06 +1000

    Add

---
+++ sudoku/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sudoku
+//
+//  Created by Ai-Lyn Tang on 26/4/20.
+//  Copyright © 2020 Ai-Lyn Tang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
